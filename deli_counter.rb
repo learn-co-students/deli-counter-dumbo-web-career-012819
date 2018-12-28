63fb3fa0c63f2fa5e61c694b41d73f3830b3feb3
andy reyes <11710reyes@gmail.com>
2018-12-28 04:55:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(array) 
+  if array.length >= 1
+    next_customer = []
+    counter = 1 
+    array.each do |name|
+      next_customer.push("#{counter}. #{name}")
+      counter += 1 
+    end 
+    puts "The line is currently: #{next_customer.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
