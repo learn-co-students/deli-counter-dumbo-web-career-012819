3f724de0711d943416047364c7e16763c825d518
andy reyes <11710reyes@gmail.com>
2018-12-29 03:40:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,3 +11,17 @@ def line(array)
     puts "The line is currently empty."
   end
 end
+
+def take_a_number(line, next_person)
+  line << next_person
+  puts "Welcome, #{next_person}. You are number #{line.length} in line."
+end
+
+def now_serving(line)
+  if line.length == 0
+    puts"There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{line[0]}."
+    line.shift
+  end
+end
